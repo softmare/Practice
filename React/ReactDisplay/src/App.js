c64acbb742e283b94b0ea9b4de5ecbaeb24ac091
Taehyeong Kim <th.imk3@gmail.com>
2022-01-22 17:32:45 +0900

    Init : create react app

---
+++ React/ReactDisplay/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Here is Softmare's react projects!
+        </p>
+        <details>
+          <summary> Play ticTacTo </summary>
+        </details>
+      </header>
+    </div>
+ );
+}
+
+export default App;
